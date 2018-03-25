9e2f96c18d010daf53e5adbcbdb3b1b8cea6e420
Noel Matta <noelmatta82@gmail.com>
2018-03-24 20:13:42 -0400

    basic routing added

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route, Link } from 'react-router-dom';
+
+class Splash extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Please log-in</h1>
+      </div>
+    )
+  }
+}
+
+class AdminView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Administrator</h1>
+      </div>
+    )
+  }
+}
+
+class UserView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome User</h1>
+      </div>
+    )
+  }
+}
 
 class App extends React.Component {
     render() {
       return (
-        <div>
-          Hello
-        </div>
+        <Router>
+          <div>
+            <Route path="/" component={Splash} />
+            <Link to="/admin">Administrator</Link> 
+            <Link to="/user">User</Link>
+            <Route path="/admin" component={AdminView} />
+            <Route path="/user" component={UserView} />
+          </div>
+        </Router>
       )
     }
 }
